ea1dcd593318a8c41dcfe11f7ece4106ee3c07d5
zzzzseong <jung48182@gmail.com>
2024-08-16 16:12:12 +0900

    보조제 상세 조회 코드 추가 * 보조제 성분 출력 부분 추가 필요

--- src/pages/supplement/container/SupplementCreateContainer.jsx
+++ src/pages/supplement/container/SupplementCreateContainer.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import {connect} from "react-redux";
-import {bindActionCreators} from "redux";
-
-import * as bodyPartCreateAction from "../store/supplementCreate";
 
 import Loading from "../../../component/Loading";
 import ConfirmModal from "../../../component/modal/ConfirmModal";
 import AlertModal from "../../../component/modal/AlertModal";
+
 import * as apiUtil from "../../../util/apiUtil";
 
 class SupplementCreateContainer extends React.Component {
@@ -16,42 +13,28 @@ class SupplementCreateContainer extends React.Component {
         this.setLoading = this.setLoading.bind(this);
         this.toggleModal = this.toggleModal.bind(this);
 
-        this.handleCreateBtnClick = this.handleCreateBtnClick.bind(this);
-
         this.setConfirmMethod = this.setConfirmMethod.bind(this);
         this.setConfirmMessage = this.setConfirmMessage.bind(this);
         this.setAlertRedirectUrl = this.setAlertRedirectUrl.bind(this);
         this.setAlertMessage = this.setAlertMessage.bind(this);
 
+        this.handleCreateBtnClick = this.handleCreateBtnClick.bind(this);
         this.handleInputChange = this.handleInputChange.bind(this);
 
-        this.ajaxBodyPartCreate = this.ajaxBodyPartCreate.bind(this);
+        this.ajaxSupplementCreate = this.ajaxSupplementCreate.bind(this);
 
         this.state = {
             loading: false,
-
             confirmMethod: null,
             confirmMessage: '',
             alertRedirectUrl: null,
             alertMessage: '',
             confirmModalIsOpen: false,
             alertModalIsOpen: false,
+
+            korName: '',
+            engName: '',
         }
-    }
-
-    shouldComponentUpdate(nextProps, nextState, nextContext) {
-        return (
-            this.state.loading !== nextState.loading
-            || this.state.confirmMethod !== nextState.confirmMethod
-            || this.state.confirmMessage !== nextState.confirmMessage
-            || this.state.alertRedirectUrl !== nextState.alertRedirectUrl
-            || this.state.alertMessage !== nextState.alertMessage
-            || this.state.confirmModalIsOpen !== nextState.confirmModalIsOpen
-            || this.state.alertModalIsOpen !== nextState.alertModalIsOpen
-
-            || this.props.korName !== nextProps.korName
-            || this.props.engName !== nextProps.engName
-        )
     }
 
     /* handlers */
@@ -63,12 +46,12 @@ class SupplementCreateContainer extends React.Component {
     }
 
     handleCreateBtnClick() {
-        this.setConfirmMessage('운동부위를 생성하시겠습니까?');
+        this.setConfirmMessage('보조제를 생성하시겠습니까?');
         this.setConfirmMethod('create');
         this.toggleModal('confirmModalIsOpen', true);
     }
     setConfirmMethod(method) {
-        if(method === 'create') this.setState({ confirmMethod: this.ajaxBodyPartCreate });
+        if(method === 'create') this.setState({ confirmMethod: this.ajaxSupplementCreate });
     }
     setConfirmMessage(message) {
         this.setState({ confirmMessage: message });
@@ -81,43 +64,42 @@ class SupplementCreateContainer extends React.Component {
     }
 
     async handleInputChange(e) {
-        const { BodyPartCreateAction } = this.props;
-
-        if(e.target.name === 'korName') await BodyPartCreateAction.setKorName(e.target.value);
-        if(e.target.name === 'engName') await BodyPartCreateAction.setEngName(e.target.value);
+        if(e.target.name === 'korName') await this.setState({ korName: e.target.value });
+        if(e.target.name === 'engName') await this.setState({ engName: e.target.value });
     }
 
     /* ajax request methods */
-    ajaxBodyPartCreate() {
-        apiUtil.sendRequest({
-            setLoading: this.setLoading,
-            url: `/api/admin/bodypart`,
-            method: apiUtil.methods.POST,
-            data: {
-                koreanName: this.props.korName,
-                englishName: this.props.engName,
-            },
-            success: (result) => {
-                this.toggleModal('confirmModalIsOpen', false);
-
-                this.setAlertMessage('운동부위 생성이 완료되었습니다.');
-                this.setAlertRedirectUrl('/bodypart/list');
-                this.toggleModal('alertModalIsOpen', true);
-            },
-            fail: (message) => {
-                this.toggleModal('confirmModalIsOpen', false);
-
-                this.setAlertMessage(`운동부위 생성에 실패했습니다.\n${message}`);
-                this.toggleModal('alertModalIsOpen', true);
-            }
-        })
+    ajaxSupplementCreate() {
+        alert('보조제 생성 요청 AJAX');
+        // apiUtil.sendRequest({
+        //     setLoading: this.setLoading,
+        //     url: `/api/admin/supplement`,
+        //     method: apiUtil.methods.POST,
+        //     data: {
+        //         koreanName: this.props.korName,
+        //         englishName: this.props.engName,
+        //     },
+        //     success: (result) => {
+        //         this.toggleModal('confirmModalIsOpen', false);
+        //
+        //         this.setAlertMessage('보조제 생성이 완료되었습니다.');
+        //         this.setAlertRedirectUrl('/supplement/list');
+        //         this.toggleModal('alertModalIsOpen', true);
+        //     },
+        //     fail: (message) => {
+        //         this.toggleModal('confirmModalIsOpen', false);
+        //
+        //         this.setAlertMessage(`보조제 생성에 실패했습니다.\n${message}`);
+        //         this.toggleModal('alertModalIsOpen', true);
+        //     }
+        // })
     }
 
     render() {
         const { confirmMethod, confirmMessage, alertRedirectUrl, alertMessage } = this.state;
         const { confirmModalIsOpen, alertModalIsOpen } = this.state;
 
-        const { korName, engName } = this.props;
+        const { korName, engName } = this.state;
 
         return (
             <React.Fragment>
@@ -125,11 +107,11 @@ class SupplementCreateContainer extends React.Component {
                 <table>
                     <tbody>
                     <tr>
-                        <td>운동 이름 (KOR)</td>
+                        <td>보조제 이름 (KOR)</td>
                         <td><input type='text' name='korName' value={korName} onChange={this.handleInputChange}/></td>
                     </tr>
                     <tr>
-                        <td>운동 이름 (ENG)</td>
+                        <td>보조제 이름 (ENG)</td>
                         <td><input type='text' name='engName' value={engName} onChange={this.handleInputChange}/></td>
                     </tr>
                     </tbody>
@@ -156,11 +138,4 @@ class SupplementCreateContainer extends React.Component {
     }
 }
 
-export default connect((state) => ({
-        korName: state.bodyPartCreate.korName,
-        engName: state.bodyPartCreate.engName,
-    }),
-    (dispatch) => ({
-        BodyPartCreateAction: bindActionCreators(bodyPartCreateAction, dispatch)
-    })
-)(SupplementCreateContainer);
+export default SupplementCreateContainer;
--- src/pages/supplement/container/SupplementDetailContainer.jsx
+++ src/pages/supplement/container/SupplementDetailContainer.jsx
@@ -1,15 +1,176 @@
 import React from 'react';
+import * as apiUtil from "../../../util/apiUtil";
+import ConfirmModal from "../../../component/modal/ConfirmModal";
+import AlertModal from "../../../component/modal/AlertModal";
+import {Link} from "@mui/material";
 
 class SupplementDetailContainer extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {}
+
+        this.setLoading = this.setLoading.bind(this);
+        this.toggleModal = this.toggleModal.bind(this);
+
+        this.handleDeleteBtnClick = this.handleDeleteBtnClick.bind(this);
+        this.setConfirmMethod = this.setConfirmMethod.bind(this);
+        this.setConfirmMessage = this.setConfirmMessage.bind(this);
+        this.setAlertRedirectUrl = this.setAlertRedirectUrl.bind(this);
+        this.setAlertMessage = this.setAlertMessage.bind(this);
+
+        this.ajaxGetSupplement = this.ajaxGetSupplement.bind(this);
+        this.ajaxDeleteSupplement = this.ajaxDeleteSupplement.bind(this);
+
+        this.state = {
+            loading: false,
+            supplement: null,
+
+            confirmMethod: null,
+            confirmMessage: '',
+            alertRedirectUrl: null,
+            alertMessage: '',
+            confirmModalIsOpen: false,
+            alertModalIsOpen: false
+        }
+    }
+
+    componentDidMount() {
+        this.ajaxGetSupplement();
+    }
+
+    setLoading(loading) {
+        this.setState({ loading: loading });
+    }
+    toggleModal(modalName, value) {
+        this.setState({ [modalName]: value });
+    }
+
+    handleDeleteBtnClick() {
+        this.setConfirmMessage('보조제를 삭제하시겠습니까?');
+        this.setConfirmMethod('delete');
+        this.toggleModal('confirmModalIsOpen', true);
+    }
+    setConfirmMethod(method) {
+        if(method === 'delete') this.setState({ confirmMethod: this.ajaxDeleteSupplement });
+    }
+    setConfirmMessage(message) {
+        this.setState({ confirmMessage: message });
+    }
+    setAlertRedirectUrl(url) {
+        this.setState({ alertRedirectUrl: url });
+    }
+    setAlertMessage(message) {
+        this.setState({ alertMessage: message });
+    }
+
+
+    ajaxGetSupplement() {
+        const { id } = this.props;
+
+        apiUtil.sendRequest({
+            setLoading: this.setLoading,
+            url: `/api/admin/supplement/${id}`,
+            method: apiUtil.methods.GET,
+            success: (result) => {
+                console.log(result);
+                this.setState({ supplement: result });
+            }
+        });
+    }
+    ajaxDeleteSupplement() {
+
     }
 
     render() {
+        const { confirmMethod, confirmMessage, alertRedirectUrl, alertMessage } = this.state;
+        const { confirmModalIsOpen, alertModalIsOpen } = this.state;
+
+        const { supplement } = this.state;
+
         return (
             <React.Fragment>
-                <h1>SupplementDetailContainer</h1>
+                { supplement != null && (
+                    <React.Fragment>
+                        {/* TODO */} {/* <button>수정</button>*/}
+                        <button onClick={this.handleDeleteBtnClick}>삭제</button>
+                        <table>
+                            <tbody>
+                            <tr>
+                                <td colSpan={2}><img src={supplement.imageURL} alt='' width={400}/></td>
+                            </tr>
+                            <tr>
+                                <td>보조제 종류</td>
+                                <td>{supplement.supplementType}</td>
+                            </tr>
+                            <tr>
+                                <td>제조사명 (KOR)</td>
+                                <td>{supplement.koreanCompanyName}</td>
+                            </tr>
+                            <tr>
+                                <td>제조사명 (ENG)</td>
+                                <td>{supplement.englishCompanyName}</td>
+                            </tr>
+                            <tr>
+                                <td>보조제 이름 (KOT)</td>
+                                <td>{supplement.koreanName}</td>
+                            </tr>
+                            <tr>
+                                <td>보조제 이름 (ENG)</td>
+                                <td>{supplement.englishName}</td>
+                            </tr>
+                            <tr>
+                                <td>맛</td>
+                                <td>{supplement.flavor} {supplement.isCaptain ? '(대표)' : null}</td>
+                            </tr>
+                            <tr>
+                                <td>마켓 링크</td>
+                                <td><Link href={supplement.marketURL} target='_blank'>{supplement.marketURL}</Link></td>
+                            </tr>
+                            <tr>
+                                <td>가격</td>
+                                <td>{supplement.price}</td>
+                            </tr>
+                            <tr>
+                                <td>용량 (1스쿱)</td>
+                                <td>{supplement.servings}</td>
+                            </tr>
+                            <tr>
+                                <td>설명</td>
+                                <td>{supplement.description}</td>
+                            </tr>
+                            <tr>
+                                <td>다른 맛</td>
+                                <td>{JSON.stringify(supplement.otherFlavors)}</td>
+                                {/*<td>{supplement.otherFlavors}</td>*/}
+                            </tr>
+                            {/*<tr>*/}
+                            {/*    <td>관련 운동 부위</td>*/}
+                            {/*    <td>{JSON.stringify(workout.bodyPartKoreanName)}</td>*/}
+                            {/*</tr>*/}
+                            {/*<tr>*/}
+                            {/*    <td>운동 영상 링크</td>*/}
+                            {/*    <td><Link href={workout.videoLink} target='_blank'>{workout.videoLink}</Link></td>*/}
+                            {/*</tr>*/}
+                            {/*<tr>*/}
+                            {/*    <td>설명</td>*/}
+                            {/*    <td>{workout.description}</td>*/}
+                            {/*</tr>*/}
+                            </tbody>
+                        </table>
+                        <ConfirmModal
+                            isOpen={confirmModalIsOpen}
+                            onRequestClose={() => {
+                                this.toggleModal('confirmModalIsOpen', false)}}
+                            confirmMethod={confirmMethod}
+                            message={confirmMessage}
+                        />
+                        <AlertModal
+                            isOpen={alertModalIsOpen}
+                            onRequestClose={()=>{this.toggleModal('alertModalIsOpen', false)}}
+                            redirectUrl={alertRedirectUrl}
+                            message={alertMessage}
+                        />
+                    </React.Fragment>
+                )}
             </React.Fragment>
         )
     }
